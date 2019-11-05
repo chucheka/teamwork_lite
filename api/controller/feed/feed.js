978c29b3985d120ac9cfed76969b51ff325d581a
ucheka22 <ryanucheka@gmail.com>
2019-11-05 08:10:40 +0100

    feature(GET feed):Build endpoint to fetch all articles and gifs [Finishes #26]

--- api/controller/feed/feed.js
+++ api/controller/feed/feed.js
@@ -1,8 +1,28 @@
+import pool from '../../config/pool';
+import { getAllArticles, getAllGifs } from '../../models/feed/feed';
 class feedController {
-	static getFeeds(req, res, next) {
-		res.status(200).json({
-			message: ' feed routes'
-		});
+	static async getFeeds(req, res, next) {
+		try {
+			let articles = await pool.query(getAllArticles);
+			let gifs = await pool.query(getAllGifs);
+			const feed = [ ...articles.rows, ...gifs.rows ].sort((a, b) => {
+				return Date.parse(b.createdOn) - Date.parse(a.createdOn);
+			});
+
+			if (feed.length > 0) {
+				return res.status(200).json({
+					status: 'success',
+					data: feed
+				});
+			} else {
+				return res.status(404).json({
+					status: 'error',
+					error: 'There are no posted articles or gifs'
+				});
+			}
+		} catch (err) {
+			console.log(err);
+		}
 	}
 }
 
---
+++ api/models/feed/feed.js
@@ -0,0 +1,2 @@
+export const getAllArticles = `SELECT * FROM articles ORDER BY "createdOn" DESC`;
+export const getAllGifs = `SELECT * FROM gifs ORDER BY "createdOn" DESC`;
