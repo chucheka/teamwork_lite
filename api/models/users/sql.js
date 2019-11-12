292a59fc18abafe91b0bd8498f49eb16fcb312ee
ucheka22 <ryanucheka@gmail.com>
2019-11-12 04:18:34 +0100

    chore(travis ci):set up travis ci for continuous integration [FINISHES #29']

--- api/models/users/sql.js
+++ api/models/users/sql.js
@@ -1,6 +1,6 @@
 import { user } from '../objects';
 
-export const userTable = `DROP TABLE IF EXISTS users CASCADE;
+export const userTable = `
 CREATE TABLE users(
     "userId" SERIAL PRIMARY KEY NOT NULL,
 	"firstName" VARCHAR(255) NOT NULL,
