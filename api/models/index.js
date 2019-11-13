03d90b135c7be104fa81657bd293bf6dec019ead
ucheka22 <ryanucheka@gmail.com>
2019-11-13 09:54:09 +0100

    refactor(codebase): refactor code base [FINISHES #30]

--- api/models/index.js
+++ api/models/index.js
@@ -1,5 +1,5 @@
 import pool from '../config/pool';
-import { userTable, createUserQuery } from './users/sql';
+import { userTable, createUserQuery, dropUserTable } from './users/sql';
 import { articlesTable, createArticleQuery } from './articles/sql';
 import { gifsTable, createGifsQuery } from './gifs/sql';
 import { commentsTable, createCommentQuery } from './comments/sql';
@@ -7,6 +7,14 @@ async function createTable(queryText, tableName) {
 	try {
 		const response = await pool.query(queryText);
 		console.log(`${tableName} table has be created`);
+	} catch (error) {
+		console.log(error.stack);
+	}
+}
+async function dropTable(queryText, tableName) {
+	try {
+		const response = await pool.query(queryText);
+		console.log(`${tableName} table has be dropped`);
 	} catch (error) {
 		console.log(error.stack);
 	}
@@ -26,10 +34,11 @@ async function queryRunner(queryText, queryValue) {
 // 		createTable(query, 'User');
 // 	}, 20000);
 // });
+dropTable(dropUserTable, 'User');
 createTable(userTable, 'User');
-createTable(articlesTable, 'Artilce');
-createTable(gifsTable, 'Gif');
-createTable(commentsTable, 'Comment');
+// createTable(articlesTable, 'Artilce');
+// createTable(gifsTable, 'Gif');
+// createTable(commentsTable, 'Comment');
 // queryRunner(createUserQuery.createUser, createUserQuery.userValues);
 // queryRunner(createArticleQuery.createArticle, createArticleQuery.articleValues);
 // queryRunner(createGifsQuery.createGif, createGifsQuery.gifValues);
