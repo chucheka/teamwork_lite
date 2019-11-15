4309296eb64d03352977cee942026be1f434abb0
ucheka22 <ryanucheka@gmail.com>
2019-11-15 11:34:25 +0100

    Update README.md

--- api/config/pool.js
+++ api/config/pool.js
@@ -3,32 +3,32 @@ import 'dotenv/config';
 
 let connect;
 
-if (process.env.NODE_ENV === 'test') {
-	connect = {
-		connectionString: process.env.TEST_DB
-	};
-} else {
-	connect = {
-		connectionString: process.env.DEV_DB || process.env.PROD_DB
-	};
-}
-// switch (process.env.NODE_ENV) {
-// 	case 'test':
-// 		connect = {
-// 			connectionString: process.env.DEV_DB
-// 		};
-// 		break;
-// 	case 'production':
-// 		connect = {
-// 			connectionString: process.env.PROD_DEV
-// 		};
-// 		break;
-// 	default:
-// 		connect = {
-// 			connectionString: process.env.DEV_DB
-// 		};
-// 		break;
+// if (process.env.NODE_ENV === 'test') {
+// 	connect = {
+// 		connectionString: process.env.TEST_DB
+// 	};
+// } else {
+// 	connect = {
+// 		connectionString: process.env.DEV_DB || process.env.PROD_DB
+// 	};
 // }
+switch (process.env.NODE_ENV) {
+	case 'test':
+		connect = {
+			connectionString: process.env.DEV_DB
+		};
+		break;
+	case 'production':
+		connect = {
+			connectionString: process.env.PROD_DEV
+		};
+		break;
+	default:
+		connect = {
+			connectionString: process.env.DEV_DB
+		};
+		break;
+}
 
 console.log(process.env.NODE_ENV);
 
